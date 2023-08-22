35e265be8feb9fa32ab589ece0ab6a442bff1b48
mihir mandal <mihirmandal620@gmail.com>
2023-08-22 16:06:32 +0530

    portfolio website

---
+++ src/data/constants.js
@@ -0,0 +1,164 @@
+export const Bio = {
+  name: "Mihir mandal",
+  roles: [
+    "Frontend Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/mihir6025",
+  resume:
+    "https://drive.google.com/file/d/11OA03ailTDjwLSqqAvh1TZmhiYUqhKuo/view?usp=drivesdk",
+  linkedin: "www.linkedin.com/in/mihir-mandal-4315951bb",
+  gmail:"mailto:mihirmandal620@gmail.com"
+};
+
+export const skills = [
+  {
+    title: "Programming",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/120px-ISO_C%2B%2B_Logo.svg.png",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+
+ 
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C510BAQGSObrO0QPlMQ/company-logo_200_200/0/1555704980721?e=1692835200&v=beta&t=FjOWcUVVNzqFbTmmp0iXQkv5keBQC5lxIQ7K6rmMSMQ",
+    role: "Open Source Contributor ",
+    company: "Hactober Fest",
+    date: "oct 2022",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "",
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Logo_vssut.svg/800px-Logo_vssut.svg.png",
+    school: "Veer surendra sai university of technology, Burla, Odisha",
+    date: "Oct 2020 - jul 2024",
+    grade: "7.61 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology at Veer surendra sai university of technology, Burla, Odisha. I have completed 6 semesters and have a CGPA of 7.61. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Information Technology",
+  },
+  {
+    id: 1,
+    img: "https://image.telanganatoday.com/wp-content/uploads/2021/09/sri-chaitanya-group_V_jpg--816x480-4g.webp?sw=1920&dsz=816x480&iw=576&p=false&r=1",
+    school: "sri chaitanya,vizag",
+    date: "Apr 2018 - Apr 2020",
+    grade: "77%",
+    desc: "I completed my class 12 high school education at sri chaitanya, Vizag, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeryPlLnQI2rkt2TJc90aYMowVjSXJEW4y0n0pbwcLFQ&s",
+    school: "Dav Public School, Chaibsa",
+    date: "Apr 2014 - Apr 2018",
+    grade: "89%",
+    desc: "I completed my class 10 education at DAV public school, Chaibasa.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "PORTFOLIO",
+    description:
+      "My Portfolio is a web application designed to showcase my portfolio. It provides a user-friendly interface for everyone to surf through my portfolio. ",
+    image:
+      "",
+    tags: [
+      "HTML",
+      "CSS",
+      "REACT js",
+    ],
+    github: "https://github.com/",
+    webapp: "",
+  },
+  // {
+  //   id: 0,
+  //   title: "Podstream",
+  //   date: "Apr 2023 - May 2023",
+  //   description:
+  //     "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+  //   image:
+  //     "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+  //   tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+  //   github: "https://github.com/rishavchanda/Podstream",
+  //   webapp: "https://podstream.netlify.app/",
+  // },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
